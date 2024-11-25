a06debd2d873cf0d32a4b8a99a632cb9f7b3540b
Youngrae Kim <youngrae@gmail.com>
2024-11-25 02:39:07 +0000

    second commit

--- dd.R
+++ dd.R
@@ -7,14 +7,14 @@ usethis::use_git_config(user.name = "Youngrae Kim", user.email = "youngrae@gmail
 ## setting: permanent storage
 credentials::credential_helper_set("manager")                 # Windows
 credentials::credential_helper_set("osxkeychain")             # Mac
-credentials::credential_helper_set("store")                   # Linux
+credentials::credential_helper_set("store")                   # Linux (RStudio Cloud)
 
 ## create a personal access token for authentication:
 usethis::create_github_token() 
 ## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
 
 ## copy and paste your token and press enter
-credentials::set_github_pat()
+credentials::set_github_pat("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")
 
 # ----------------------------------------------------------------------------
 
