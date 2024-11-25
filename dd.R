c6bd508806ff0485105dee8c34b348d06fd353de
Youngrae Kim <youngrae@gmail.com>
2024-11-25 02:36:05 +0000

    first commit

---
+++ dd.R
@@ -0,0 +1,27 @@
+install.packages("usethis")
+
+
+## Name, email
+usethis::use_git_config(user.name = "Youngrae Kim", user.email = "youngrae@gmail.com")
+
+## setting: permanent storage
+credentials::credential_helper_set("manager")                 # Windows
+credentials::credential_helper_set("osxkeychain")             # Mac
+credentials::credential_helper_set("store")                   # Linux
+
+## create a personal access token for authentication:
+usethis::create_github_token() 
+## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
+
+## copy and paste your token and press enter
+credentials::set_github_pat()
+
+# ----------------------------------------------------------------------------
+
+#### 4. Restart R! ###########################################################
+
+# ----------------------------------------------------------------------------
+
+#### 5. Verify settings ######################################################
+
+usethis::git_sitrep()
